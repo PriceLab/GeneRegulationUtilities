17965e908810f6f01204089606a74b467490563d
paul-shannon <paul.thurmond.shannon@gmail.com>
2016-02-23 14:47:20 -0800

    added findSNPsInFootprints, rationaled tbl.fpAnnotated

--- R/findTFsInFootprints.R
+++ R/findTFsInFootprints.R
@@ -1,5 +1,6 @@
 # findTFsInFootprints.R
 #------------------------------------------------------------------------------------------------------------------------
+library(GenomicRanges)
 library(RUnit)
 #------------------------------------------------------------------------------------------------------------------------
 printf <- function(...) print(noquote(sprintf(...)))
@@ -16,15 +17,20 @@ printf <- function(...) print(noquote(sprintf(...)))
 #   }
 #
 #------------------------------------------------------------------------------------------------------------------------
-findTFsInFootprints <- function(tbl.fp, tbl.motifToGenes, chromosome, startLoc, endLoc, transcriptionFactors)
+findTFsInFootprints <- function(tbl.fp, tbl.motifToGenes, chromosome, startLoc, endLoc, transcriptionFactors=NA)
 {
-    tbl.sub <- subset(tbl.fp, chr==chromosome & start >= startLoc & end <= endLoc)
+    if(all(is.na(transcriptionFactors)))
+        transcriptionFactors <- sort(unique(tbl.motifToGenes$tfs))
+
+    tbl.sub <- subset(tbl.fp, chr==chromosome & mfpStart >= startLoc & mfpEnd <= endLoc)
     motifs.per.fp <- tbl.sub$name
     tbl.hits <- subset(tbl.motifToGenes, motif %in% motifs.per.fp)
+    indicesMatched <- match(transcriptionFactors, tbl.hits$tfs)
     tbl.hitAndMatched <- subset(tbl.hits, tfs %in% transcriptionFactors)
     motifs <- unique(tbl.hitAndMatched$motif)
     tbl.sub <- subset(tbl.sub, name %in% motifs)
 
+    #browser()
     tfs.per.motif <- lapply(motifs, function(m) subset(tbl.hitAndMatched, motif==m)$tfs)
     names(tfs.per.motif) <- motifs
     tfsPerRow <- unlist(lapply(tbl.sub$name, function(motifName)
@@ -32,6 +38,66 @@ findTFsInFootprints <- function(tbl.fp, tbl.motifToGenes, chromosome, startLoc, 
     tbl.augmented <- cbind(tbl.sub, tfsMatched=tfsPerRow, stringsAsFactors=FALSE)
 
     invisible(tbl.augmented)
+
+} # findTFsInFootprints
+#------------------------------------------------------------------------------------------------------------------------
+# identify some proximal upstream snps, within
+# with mef2c at hg39 chr5:88,716,241-88,906,105, coded on the minus strand, here are some proximal upstream snps
+# reported by mariette
+#
+# true overlaps with mfpStart-mfpEnd: rs3814428, rs770463, rs446500
+# near misses: rs10038371 rs214136 rs16876775
+#
+# fivenum(tbl$GRCh38.liftover) [1] 88640561 88740812 88828020 88921378 89001975
+#   dim(subset(tbl, GRCh38.liftover > 88906105))  [1] 43 18
+#   tbl.upstream <- tbl[which(tbl$GRCh38.liftover - 88906105 > 0),]   # 43 18
+#   upstream.loc <- tbl.upstream$GRCh38.liftover - 88906105
+# head(tbl.upstream[, c(1,3,4,5,6,17:19)], n=20)
+#     Symbol CHR        SNP       BP A1 GRCh38.liftover   GRCh37 upstream
+# 115  MEF2C   5 rs10085009 88241752  A        88910179 88205996     4074
+# 150  MEF2C   5 rs13357047 88246411  A        88914838 88210655     8733
+# 119  MEF2C   5   rs770466 88251210  C        88919637 88215454    13532
+# 81   MEF2C   5   rs304132 88251350  A        88919777 88215594    13672
+# 153  MEF2C   5 rs10044186 88254553  C        88922980 88218797    16875
+# 26   MEF2C   5 rs10037047 88255720  G        88924147 88219964    18042
+# 44   MEF2C   5  rs190982* 88259176  G        88927603 88223420    21498
+# 42   MEF2C   5   rs301717 88267164  A        88935591 88231408    29486
+# 30   MEF2C   5   rs301718 88267595  G        88936022 88231839    29917
+# 34   MEF2C   5   rs301719 88268671  C        88937098 88232915    30993
+# 40   MEF2C   5   rs301720 88269410  A        88937837 88233654    31732
+# 35   MEF2C   5   rs301723 88271430  A        88939857 88235674    33752
+# 36   MEF2C   5   rs301725 88272216  T        88940643 88236460    34538
+# 29   MEF2C   5  rs7700950 88272698  T        88941125 88236942    35020
+# 33   MEF2C   5   rs301726 88272826  T        88941253 88237070    35148
+# 31   MEF2C   5   rs301727 88273286  G        88941713 88237530    35608
+# 32   MEF2C   5   rs301728 88273557  T        88941984 88237801    35879
+# 39   MEF2C   5   rs301729 88273797  G        88942224 88238041    36119
+# 41   MEF2C   5  rs1650626 88274962  G        88943389 88239206    37284
+# 37   MEF2C   5  rs1650627 88275084  G        88943511 88239328    37406
+findSNPsInFootprints <- function(tbl.fp, tbl.motifToGenes,
+                                 chromosome, region.startLoc, region.endLoc,
+                                 snp.chromosome, snp.loc,
+                                 transcriptionFactors=NA,
+                                 padding=0)
+{
+    tbl.fpSub <- findTFsInFootprints(tbl.fp, tbl.motifToGenes,
+                                     chromosome, region.startLoc, region.endLoc,
+                                     transcriptionFactors)
+    #browser()
+    if(nrow(tbl.fpSub) == 0)
+        return(data.frame())
+
+    gr.fp <- with(tbl.fpSub, GRanges(seqnames=chr, IRanges(start=mfpStart, end=mfpEnd)))
+    gr.snp <- GRanges(seqnames=snp.chromosome, IRanges(start=snp.loc-padding, end=snp.loc+padding))
+    #browser()
+    tbl.overlaps <- as.data.frame(findOverlaps(gr.fp, gr.snp))
+    snp <- rep(-1, length(gr.fp))
+    snp[tbl.overlaps$queryHits] <- snp.loc[tbl.overlaps$subjectHits]
+
+    tbl.fpSub$snp <- snp
+    result <- subset(tbl.fpSub, snp != -1)
+    #browser()
+    result[, c("chr", "mfpStart", "mfpEnd", "motifStart", "motifEnd", "sequence", "name", "snp", "tfsMatched")]
 
 } # findTFsInFootprints
 #------------------------------------------------------------------------------------------------------------------------
--- inst/unitTests/test_GeneRegulationUtilities.R
+++ inst/unitTests/test_GeneRegulationUtilities.R
@@ -19,6 +19,9 @@ runTests <- function()
    test_1_footprint_two_tfs()
    test_emptyResults()
    test_360k_chr5_alzheimers_region()
+   test_findSNPsInFootprints()
+   test_findSNPsNearFootprints()
+   test_LXH1_matching()
 
 } # runTests
 #------------------------------------------------------------------------------------------------------------------------
@@ -32,16 +35,16 @@ test_1_footprint_SAT2B_tf <- function()
 {
   printf("--- test_1_footprint_SAT2B_tf")
 
-  loc <- tbl.fpAnnotated[grep("MA0679", tbl.fpAnnotated$info)[1], c("chr", "start", "end")]
+  loc <- tbl.fpAnnotated[grep("MA0679", tbl.fpAnnotated$name)[1], c("chr", "mfpStart", "mfpEnd")]
   target.tf <- "SATB2"
   tbl <- findTFsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
-                         chromosome=loc$chr, startLoc=loc$start, endLoc=loc$end,
-                         transcriptionFactors=target.tf)
+                             chromosome=loc$chr, startLoc=loc$mfpStart, endLoc=loc$mfpEnd,
+                             transcriptionFactors=target.tf)
 
     # tbl.fpAnnotated identifies 3 motifs in this one footprint, and each of them is associated with SATB2
     # we therefore expect 3 rows, each describing the same footprint, and each with a different motif
 
-  checkEquals(dim(tbl), c(3, 17))
+  checkEquals(dim(tbl), c(3, 18))
   checkEquals(tbl$tfsMatched, rep("SATB2", 3))
   checkEquals(tbl$name, c("MA0679.1", "MA0756.1", "MA0757.1"))
 
@@ -51,16 +54,16 @@ test_1_footprint_two_tfs <- function()
 {
   printf("--- test_1_footprint_two_tfs")
 
-  loc <- tbl.fpAnnotated[grep("MA0679", tbl.fpAnnotated$info)[1], c("chr", "start", "end")]
+  loc <- tbl.fpAnnotated[grep("MA0679", tbl.fpAnnotated$name)[1], c("chr", "mfpStart", "mfpEnd")]
   target.tf <- c("SATB2", "CUX1")
   tbl <- findTFsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
-                         chromosome=loc$chr, startLoc=loc$start, endLoc=loc$end,
-                         transcriptionFactors=target.tf)
+                             chromosome=loc$chr, startLoc=loc$mfpStart, endLoc=loc$mfpEnd,
+                             transcriptionFactors=target.tf)
 
     # tbl.fpAnnotated identifies 3 motifs in this one footprint, and each of them is associated with SATB2
     # we therefore expect 3 rows, each describing the same footprint, and each with a different motif
 
-  checkEquals(dim(tbl), c(3, 17))
+  checkEquals(dim(tbl), c(3, 18))
   checkEquals(tbl$tfsMatched, rep("SATB2;CUX1", 3))
   checkEquals(tbl$name, c("MA0679.1", "MA0756.1", "MA0757.1"))
 
@@ -81,20 +84,20 @@ test_emptyResults <- function()
 
       # now repeat the SATB2 search (see above) with locs just 1 base too small, by incrementing start
 
-   loc <- tbl.fpAnnotated[grep("MA0679", tbl.fpAnnotated$info)[1], c("chr", "start", "end")]
+   loc <- tbl.fpAnnotated[grep("MA0679", tbl.fpAnnotated$name)[1], c("chr", "motifStart", "motifEnd")]
    loc.orig <- loc
-   loc$start <- loc$start + 1
-   target.tf <- "SATB2"   # mapped to the largest number of motifs in tbl.motifToMultipleGenes
+   loc$motifStart <- loc$motifStart + 1
+   target.tf <- "SATB2"
    tbl <- findTFsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
-                          chromosome=loc$chr, startLoc=loc$start, endLoc=loc$end,
+                          chromosome=loc$chr, startLoc=loc$motifStart, endLoc=loc$motifEnd,
                           transcriptionFactors=target.tf)
    checkEquals(nrow(tbl), 0)
 
       # now repeat the SATB2 search (see above) with locs just 1 base too small, by decrementing end
 
    loc <- loc.orig
-   loc$end <- loc$end - 1
-   target.tf <- "SATB2"   # mapped to the largest number of motifs in tbl.motifToMultipleGenes
+   loc$motifEnd <- loc$motifEnd - 1
+   target.tf <- "SATB2"
    tbl <- findTFsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
                           chromosome=loc$chr, startLoc=loc$start, endLoc=loc$end,
                           transcriptionFactors=target.tf)
@@ -112,8 +115,180 @@ test_360k_chr5_alzheimers_region <- function()
    tbl <- findTFsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
                               chromosome=loc$chr, startLoc=loc$start, endLoc=loc$end,
                               transcriptionFactors=target.tfs)
-   checkEquals(dim(tbl), c(212, 17))
+   checkEquals(dim(tbl), c(212, 18))
    checkEquals(sort(unique(tbl$tfsMatched)), c("HLF", "POU5F2", "POU5F2;SOX4", "SATB2;CUX2", "SOX4"))
 
 } # test_360k_chr5_alzheimers_region
+#------------------------------------------------------------------------------------------------------------------------
+test_findSNPsInFootprints <- function()
+{
+   printf("--- test_findSNPsInFootprints")
+   snp.chromosome <- "chr5"
+   snp.loc.1 <- 88884587  # subset(tbl.snps, SNP %in% c("rs3814428", "rs770463", "rs446500"))$GRCh38.liftover
+   snp.loc.3 <- 88883758  # from 1000 genomes, should retrieve 6-motif footprint
+
+   tfs.all <- sort(unique(tbl.motifToMultipleGenes$tfs))
+   tfs <- c("ZNF263", "MAZ", "KLF16")
+
+       # search region
+   region.chromosome <- "chr5"
+   region.startLoc <- 88884570
+   region.endLoc   <- 88884600   # chr5:88884570-88884600
+
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               region.chromosome, region.startLoc, region.endLoc,
+                               snp.chromosome, snp.loc.1,
+                               transcriptionFactors=tfs)
+
+   checkEquals(dim(tbl), c(7, 9))
+   checkEquals(sort(unique(tbl$tfsMatched)), c("KLF16", "MAZ", "ZNF263"))
+
+      # now retrieve a snp in a footprint, but notice that it misses the reported motif sequence
+   snp.loc.2 <- 88899133  # subset(tbl.snps, SNP %in% c("rs3814428", "rs770463", "rs446500"))$GRCh38.liftover
+   region.startLoc <- 88899100
+   region.endLoc   <- 88899300
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               region.chromosome, region.startLoc, region.endLoc,   # region of interest
+                               snp.chromosome, snp.loc.2,
+                               transcriptionFactors="TCF7")
+   checkEquals(dim(tbl), c(1,9))
+   x <- as.list(tbl[1,])
+   checkEquals(x$chr, "chr5")
+   checkEquals(x$mfpStart, 88899109)
+   checkEquals(x$mfpEnd, 88899140)
+   checkEquals(x$motifStart, 88899109)
+   checkEquals(x$motifEnd, 88899120)
+   checkEquals(x$sequence, "AAAGTTCAAAGC")
+   checkEquals(x$name, "MA0769.1")
+   checkEquals(x$snp, 88899133)
+   checkEquals(x$tfsMatched, "TCF7")
+
+       # now try combining the two snp.locs successfully tested above
+   tfs <- c("KLF16", "MAZ", "ZNF263", "TCF7")
+   region.startLoc <- 88884570
+   region.endLoc   <- 88899300
+
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               region.chromosome, region.startLoc, region.endLoc,   # region of interest
+                               snp.chromosome, c(snp.loc.1, snp.loc.2),
+                               transcriptionFactors=tfs)
+
+   checkEquals(dim(tbl), c(8, 9))
+   checkEquals(sort(unique(tbl$tfsMatched)), c("KLF16", "MAZ", "TCF7", "ZNF263"))
+   checkEquals(sort(unique(tbl$snp)), c(88884587, 88899133))
+
+
+} # test_findSNPsInFootprints
+#------------------------------------------------------------------------------------------------------------------------
+# snp rs10038371 is just upstream  chr5:88,903,407 of a footprint+motif at 88,903,378-403 (upstream from the perspective of the
+# possibly regulated gene, MEF2C, which is encoded on the minus strand).
+# here we look for that snp near that footprint, passing in a padding parameter which produces a liberal
+# notion of overlapping
+test_findSNPsNearFootprints <- function()
+{
+   printf("--- test_findSNPsNearFootprints")
+
+   snp.chromosome <- "chr5"
+   snp.loc <- 88903407
+   target.region.chromosome <- "chr5"
+   target.region.startLoc <- 88903000
+   target.region.endLoc   <- 88903500
+
+   tfs.all <- sort(unique(tbl.motifToMultipleGenes$tfs))
+   #tfs <- c("KLF16", "MAZ", "ZNF263", "TCF7")
+
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               target.region.chromosome, target.region.startLoc, target.region.endLoc,
+                               snp.chromosome, snp.loc,
+                               transcriptionFactors=NA, # tfs.all,
+                               padding=0)
+   checkEquals(nrow(tbl), 0)
+
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               target.region.chromosome, target.region.startLoc, target.region.endLoc,
+                               snp.chromosome, snp.loc,
+                               transcriptionFactors=NA, # tfs.all,
+                               padding=5)
+   checkEquals(dim(tbl), c(1,9))
+
+     # tfsMatched in the above table: ISL1;ISL2;LHX1;LHX2;LHX3;LHX4;LHX5;LHX6;LHX8;LHX9
+
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               target.region.chromosome, target.region.startLoc, target.region.endLoc,
+                               snp.chromosome, snp.loc,
+                               transcriptionFactors="ISL1",
+                               padding=5)
+   checkEquals(dim(tbl), c(1, 9))
+
+     # with very large padding, our snp rs10038371 "overlaps" with the footprint at 88,902,507:88,902534
+     # though of no likely biological interest, this helps to test the padding calculation
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               target.region.chromosome, target.region.startLoc-1000, target.region.endLoc,
+                               snp.chromosome, snp.loc,
+                               transcriptionFactors=NA, #tfs.all,
+                               padding=1000)
+
+   checkEquals(dim(tbl), c(2, 9))
+
+} # test_findSNPsNearFootprints
+#------------------------------------------------------------------------------------------------------------------------
+# a cryptic and transient bug, now no longer evident.
+# originally, with all possible transcription factors considered, LHX1 was found (along with others in its gene family)
+# for the region looked at here.
+# but when LHX1 was considered alone, the results came back empty.
+# this function ensures that they come back properly: one row describing an intersecting snp/fp on chr5 just upstream
+# on the minus strand from mef2c
+test_LXH1_matching <- function()
+{
+   printf("--- test_LXH1_matching")
+
+   snp.chromosome <- "chr5"
+   snp.loc <- 88903407
+   target.region.chromosome <- "chr5"
+   target.region.startLoc <- 88903000
+   target.region.endLoc   <- 88903500
+
+   tfs.all <- sort(unique(tbl.motifToMultipleGenes$tfs))
+
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               target.region.chromosome, target.region.startLoc, target.region.endLoc,
+                               snp.chromosome, snp.loc,
+                               transcriptionFactors=NA, # tfs.all,
+                               padding=5)
+
+   checkEquals(dim(tbl), c(1,9))
+   target.tf <- "LHX1"
+   checkTrue(grepl(target.tf, tbl[1, "tfsMatched"]))
+   tokens <- strsplit(tbl[1, "tfsMatched"], ";")[[1]]
+   checkTrue(match(target.tf, tokens) > 0)
+
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               target.region.chromosome, target.region.startLoc, target.region.endLoc,
+                               snp.chromosome, snp.loc,
+                               transcriptionFactors=target.tf,
+                               padding=5)
+   checkEquals(dim(tbl), c(1,9))
+
+} # test_LHX1_matching
+#------------------------------------------------------------------------------------------------------------------------
+explore_abca7_snps <- function()
+{
+    printf("--- explore_abca7_snps")
+    f <- system.file(package="GeneRegulationUtilities", "extdata", "abca7-associated-snps-1kg.tsv")
+    stopifnot(file.exists(f))
+    tbl.snps <- read.table(f, header=FALSE, sep="\t", as.is=TRUE)
+    colnames(tbl.snps) <- c("chrom", "loc")
+    displayBedTable(igv, tbl.snps[, c(1,2,2)], "1kg snps")
+    min.loc <- min(tbl.snps$loc)
+    max.loc <- max(tbl.snps$loc)
+    tbl.sub <- subset(tbl.fpAnnotated, chr=="chr19" & mfpStart >= (min.loc-500) & mfpEnd <= (max.loc + 1600))
+    displayBedTable(igv, tbl.sub[, c("chr", "mfpStart", "mfpEnd")], "footprints")
+
+   tbl <- findSNPsInFootprints(tbl.fpAnnotated, tbl.motifToMultipleGenes,
+                               "chr19", min.loc, max.loc,
+                               "chr19", sort(unique(tbl.snps$loc)),
+                               transcriptionFactors=NA,
+                               padding=10)
+
+} # explore_abca7_snps
 #------------------------------------------------------------------------------------------------------------------------
